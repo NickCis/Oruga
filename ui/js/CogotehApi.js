3ac9fbd5bf74e237a483de960cfb4cf4758f4d78
Nicolas Cisco <n.cis_92@hotmail.com>
2011-12-22 02:11:52 -0300

    Using custom jquery.ui.slider to have full value control

--- ui/js/CogotehApi.js
+++ ui/js/CogotehApi.js
@@ -4,22 +4,34 @@ var Cogoteh = function() {
 
 (function () {
 	Cogoteh.prototype.setPlayer = function (player) {
-		if (Cogoteh[player])
+		if (this[player])
 			this.player = player;
 		else
 			this.player = 'cogoteh';
 	};
 
 	Cogoteh.prototype.getArtist = function () {
+		//Arguments: cb, data
+		// cb:the callback where the list of dicts is passed [{'id': , 'artist':, 'img':}, ]
+		// data: information passed to filter the artists, if nothing is passed, all artists are returned
 		this[this.player].getArtist.apply(this, arguments); 
 	};
 	Cogoteh.prototype.getAlbum = function () {
+		//Arguments: cb, data
+		// cb:the callback where the list of dicts is passed [{'id': , 'album':, 'idartist':, 'artist':,'img':}, ]
+		// data: information passed to filter the albums, if nothing is passed, all albums are returned
 		this[this.player].getAlbum.apply(this, arguments); 
 	};
 	Cogoteh.prototype.getSong = function () {
+		//Arguments: cb, data
+		// cb:the callback where the list of dicts is passed [{'id': , 'title':, 'idartist':, 'artist':, 'idalbum':,'album':,'genre':,'year':,'tracknum':,'time':,'img':}, ]
+		// data: information passed to filter the songs, if nothing is passed, all songs are returned
 		this[this.player].getSong.apply(this, arguments); 
 	};
 	Cogoteh.prototype.getPlaylist = function () {
+		//Arguments: cb, data
+		// cb:the callback where the list of dicts is passed [{'id': , 'album':, 'idartist':, 'artist':,}, ]
+		// data: information passed to filter the playlists, if nothing is passed, all playlists are returned
 		this[this.player].getPlaylist.apply(this, arguments); 
 	};
 	Cogoteh.prototype.getCurrent = function () {
@@ -35,13 +47,16 @@ var Cogoteh = function() {
 		this[this.player].pause.apply(this, arguments); 
 	};
 	Cogoteh.prototype.addSongToPlaylist = function() {
+		//Arguments data, playlistid
+		// data: information of the songs to be added, every song that matchs will be added
+		// playlistid: id of the playlist
+		//if playlist isnt setted, the song will be added to the current playable playlist
 		this[this.player].addSongToPlaylist.apply(this, arguments);
 	};
 
 
 	Cogoteh.prototype.test = {};
 	Cogoteh.prototype.test.getArtist = function(cb, artist) {
-		//Formato id, artist
 		cb( [
 			{'id': 0, 'artist': 'Pepe', 'img': 'images/person-16.png'},
 			{'id': 1, 'artist': 'Addd', 'img': 'images/person-16.png'},
@@ -51,7 +66,6 @@ var Cogoteh = function() {
 		]);
 	};
 	Cogoteh.prototype.test.getAlbum = function(cb, data) {
-		//Formato id, album, idartist, artist
 		var albums = [
 			{'id': 0, 'album': 'Caja', 'idartist':0,  'artist': 'Pepe', 'img': 'images/person-16.png' },
 			{'id': 1, 'album': 'Carton', 'idartist':0,  'artist': 'Pepe', 'img': 'images/person-16.png'},
@@ -73,7 +87,6 @@ var Cogoteh = function() {
 		cb(ret );
 	};
 	Cogoteh.prototype.test.getSong = function(cb, song) {
-		//Formato de rta de cancion id, title, idartist, artist, idalbum, album, genre, discnum, year, tracknum, time
 		cb( [
 			{'id': 0, 'title':'Peron', 'idartist': 0, 'artist': 'Pepe', 'idalbum': 0, 'album': 'Caja', 'genre': '', 'dicnum': 0, 'year':2010, 'tracknum': 4, 'time': 200, 'img': 'images/person-16.png'},
 			{'id': 1, 'title':'Papapa', 'idartist': 0, 'artist': 'Pepe', 'idalbum': 0, 'album': 'Caja', 'genre': '', 'dicnum': 0, 'year':2010, 'tracknum': 4, 'time': 200, 'img': 'images/person-16.png'},
@@ -82,7 +95,6 @@ var Cogoteh = function() {
 		]);
 	};
 	Cogoteh.prototype.test.addSongToPlaylist = function(data, playlistid) {
-		//if playlist isnt setted, the song will be added to the current playable playlist
 		if (! this.playlist)
 			this.playlist = [];
 		this.playlist.push(data);
