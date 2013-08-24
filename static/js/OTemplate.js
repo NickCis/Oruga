82e68fbc384cc6137cecdd15273d72338ea74c53
Nicolas Cisco <n.cis_92@hotmail.com>
2013-08-24 07:21:10 -0300

    Se agrega compass. Se avanza con la interaccion de la interfaz, andan botones del main

--- static/js/OTemplate.js
+++ static/js/OTemplate.js
@@ -4,11 +4,20 @@
 	*/
 	function $Ui(config){
 		$O.call(this, config);
-		this._d = document.createElement('div');
+		if(this.c.container)
+			this._d = (typeof(this.c.container) == "string") ? document.getElementById(this.c.container) : this.c.container;
+		this._d = this._d || document.createElement('div');
 	};
 
 	$Ui.prototype = $O.f.xD($O.prototype, {
 		constructor: $Ui,
+		getElesByClass: function(clss, cb){
+			var eles = this._d.getElementsByClassName(clss);
+			if(eles.length){
+				for(var i=0, ele; ele = eles[i]; i++)
+					cb.call(this, ele);
+			}
+		},
 		basicLayout: function(){ 
 			$O.f.notImplemented.call(this, "basicLayout");
 		}
--- static/js/widgets/Main.js
+++ static/js/widgets/Main.js
@@ -8,6 +8,50 @@
 		basicLayout: function(){
 			this._d.style.height = "100%";
 			this._d.innerHTML = window.render.orugaMain();
+
+			this.getElesByClass('btn-navbar', function(ele){
+				ele.addEventListener('click', this.toggleView.bind(this), false);
+			});
+
+			this.getElesByClass('main-search-form', function(ele){
+				ele.addEventListener('submit', this.search.bind(this), false);
+			});
+
+			this.getElesByClass('main-nav-ul', function(ele){
+				ele.addEventListener('click', this.toggleDivBody.bind(this), false);
+			});
+		},
+		toggleView: function(){
+			this.getElesByClass('nav-collapse', function(ele){
+				ele.style.height = (!ele.style.height || parseInt(ele.style.height, 10) == 0) ? "auto" : "0px";
+			});
+		},
+		search: function(event){
+			event.preventDefault();
+
+
+			return true;
+		},
+		toggleDivBody: function(event){
+			event.preventDefault();
+			var ele = event.target;
+			this.getElesByClass('main-nav-ul', function(ele){
+				for(var i=0, child, children = ele.children; child = children[i]; i++)
+					child.classList.remove('active');
+			});
+			ele.parentNode.classList.add('active');
+
+			var dataTarget = ele.getAttribute('data-target');
+
+			this.getElesByClass('main-body-div', function(ele){
+				if(ele.classList.contains(dataTarget))
+					ele.classList.remove("no-show");
+				else
+					ele.classList.add("no-show");
+			});
+
+
+			return true;
 		}
 	});
 
